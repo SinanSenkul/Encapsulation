c6d50e8ce717cdb07b0d50ae429fac85f3e4ed5f
sinan <sinan@SINAN-SENKUL>
2024-08-11 19:52:50 +0300

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿class Car
+{
+    //fields
+    public string _brand;
+    public string _model;
+    public string _color;
+    private int _dourNum;
+
+    //properties
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    private int DourNum
+    {
+        get
+        {
+            return _dourNum;
+        }
+        set
+        {
+            if (value != 2 || value != 4)
+            {
+                Console.WriteLine("Yanlış bir değer girildi...");
+                _dourNum = -1;
+            }
+            else
+            {
+                _dourNum = value;
+            }
+        }
+    }
+}
